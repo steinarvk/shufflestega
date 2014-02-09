34da4975b3cc9557a26f16de156d207470eb4bec
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2014-02-09 20:47:23 +0100

    adding some of the widgets that will be relevant

--- src/Frontend.js
+++ src/Frontend.js
@@ -5,14 +5,22 @@ var Stega = require("./Stega"),
 require( "jquery-ui" );
 
 $( document ).on( "ready", function() {
-    var i = 0, root = $("#shufflestega");
-
-    root.html( "" );
-    root.sortable();
-    root.disableSelection();
+    var i = 0,
+        app = ich.app( {} ),
+        root = $("#shufflestega")
+            .html( app ),
+        spinner = $("#stega_sequencelength")
+            .spinner(),
+        actives = $("#stega_sequence_active")
+            .sortable()
+            .disableSelection(),
+        inactives = $("#stega_sequence_inactive"),
+        capacity = $("#stega_capacitybar")
+                     .progressbar( {value: 35} );
 
     for(i = 0; i < 52; i++) {
         $( ich.entry( {number: i + 1} ) )
-            .appendTo( root );
+            .toggleClass( "ordered-box-inactive", i%3 !== 0 )
+            .appendTo( (i%3 === 0) ? inactives : actives );
     }
 } );
