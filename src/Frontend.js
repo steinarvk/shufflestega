7bf9c3ef0c724eff12eaea81405af34ad89e452a
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2014-02-09 22:55:59 +0100

    interaction between the two sequence displays
    Enter with the mouse/touch, or enter by
    copy-pasting into the text box.

--- src/Frontend.js
+++ src/Frontend.js
@@ -1,26 +1,144 @@
 var Stega = require("./Stega"),
     ich = require("icanhaz"),
-    $ = require( "jquery" );
+    $ = require( "jquery" ),
+    Bignum = require( "./Bignum" );
 
 require( "jquery-ui" );
 
 $( document ).on( "ready", function() {
-    var i = 0,
-        app = ich.app( {} ),
+    var app = ich.app( {} ),
+        currentN = 52,
         root = $("#shufflestega")
             .html( app ),
         spinner = $("#stega_sequencelength")
-            .spinner(),
+            .spinner({
+                stop: onSequenceLengthSpinnerChanged
+            })
+            .change( onSequenceLengthSpinnerChanged ),
         actives = $("#stega_sequence_active")
-            .sortable()
+            .sortable({
+                update: function( event, ui ) {
+                    ui.item.trigger( "stega-activate" );
+
+                    updateSequenceText();
+                }
+            })
             .disableSelection(),
-        inactives = $("#stega_sequence_inactive"),
+        seqtext = $("#stega_sequencetext")
+            .keyup( onTextAreaChange )
+            .change( onTextAreaChange ),
         capacity = $("#stega_capacitybar")
                      .progressbar( {value: 35} );
-
-    for(i = 0; i < 52; i++) {
-        $( ich.entry( {number: i + 1} ) )
-            .toggleClass( "ordered-box-inactive", i%3 === 0 )
-            .appendTo( (i%3 === 0) ? inactives : actives );
+    
+    function onSequenceLengthSpinnerChanged() {
+        var v = parseInt( spinner.val() );
+        setSequenceLength( v );
     }
+
+    function setSequenceLength( n ) {
+        if( typeof n !== "number" || (n%1) !== 0 ) {
+            return false;
+        }
+
+        if( n < 2 || n > 500 ) {
+            return false;
+        }
+
+        if( n === currentN ) {
+            return false;
+        }
+
+        resetOrdering( n );
+
+        return true;
+    }
+
+    function onTextAreaChange( evt ) {
+        function onInvalid() {
+            seqtext.addClass( "invalid" );
+        }
+
+        function onValid() {
+            seqtext.removeClass( "invalid" );
+        }
+
+        var rv;
+
+        try {
+            rv = seqtext.val().split(",").map( function(x) {
+                return parseInt( x );
+            } );
+
+            if( rv.length !== currentN ) {
+                throw new Error( "ordering not valid" );
+            }
+
+            if( !Bignum.validOrdering( rv ) ) {
+                throw new Error( "ordering not valid" );
+            }
+
+            onValid();
+
+            setOrdering( rv );
+
+            $(".ordered-box").trigger("stega-activate");
+        }
+        catch(err) {
+            onInvalid();
+        }
+    };
+
+    function getOrdering() {
+        return actives.sortable("toArray", {attribute: "data-value"})
+            .map( function(x) {
+                return parseInt( x );
+            } );
+    }
+
+    function updateSequenceText() {
+        seqtext.val( getOrdering().toString() );
+    }
+
+    function makeCard( i ) {
+        var rv = $( ich.entry( {index: i,
+                                displaynumber: i + 1} ) );
+        
+        return rv.addClass( "ordered-box-inactive" )
+            .bind( "stega-activate", function() {
+                rv.removeClass( "ordered-box-inactive" );
+                return true;
+            } )
+            .click( function() {
+                rv.trigger("stega-activate")
+                    .remove().appendTo( actives );
+
+                updateSequenceText();
+            } );
+    }
+
+    function resetOrdering( n ) {
+        var rv = [], i;
+        currentN = n;
+        for(i = 0; i < n; i++) {
+            rv.push( i );
+        }
+        return setOrdering( rv );
+    }
+
+    function setOrdering( ordering ) {
+        var i;
+
+        if( getOrdering() === ordering ) {
+            return;
+        }
+
+        actives.empty();
+        for(i = 0; i < ordering.length; i++) {
+            makeCard( ordering[i] ).appendTo( actives );
+        }
+
+        updateSequenceText();
+    }
+
+    resetOrdering( 52 );
 } );
