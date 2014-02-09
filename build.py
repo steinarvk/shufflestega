e004f4981f76f1c0cf535f0eef348d3eb44a179a
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2014-02-09 18:02:09 +0100

    adding ICanHaz for frontend templating
    The build script now searches the
    template/pagelet/ directory for HTML files
    and add them to the generated HTML file.
    
    We can then render them (using Mustache)
    from the Javascript with ICanHaz.

--- build.py
+++ build.py
@@ -3,16 +3,34 @@ import os.path
 import subprocess as sp
 import sys
 import errno
+import codecs
+import os
+import re
 
 def contents( fn ):
-    with open( fn ) as f:
+    with codecs.open( fn, "r", "utf8" ) as f:
         return f.read()
 
 def inline_js_block(code):
-    return """<script type="text/javascript">{}</script>""".format( code )
+    return u"""<script type="text/javascript">{}</script>""".format( code )
 
 def inline_css_block(code):
-    return """<style type="text/css">{}</style>""".format( code )
+    return u"""<style type="text/css">{}</style>""".format( code )
+
+def inline_ich_block(name, code):
+    rv = u"""<script id="{}" type="text/html">{}</script>"""
+    return rv.format(name, code)
+
+def inline_ich_blocks_from_dir(ichdir):
+    fnre = re.compile( r"(.*)\.html$" )
+    rv = []
+    for filename in os.listdir( ichdir ):
+        m = fnre.match( filename )
+        if m:
+            name = m.groups()[ 0 ]
+            code = contents( os.path.join( ichdir, filename ) )
+            rv.append( inline_ich_block( name, code ) )
+    return "\n\n".join( rv )
 
 def sibling_filename( base, name ):
     head, tail = os.path.split( base )
@@ -26,30 +44,32 @@ def mkbasedir( path ):
         if not (exc.errno == errno.EEXIST and os.path.isdir( head )):
             raise
 
-def render_single_file( outname, template, js, css ):
+def render_single_file( outname, template, ich, js, css ):
     code = inline_js_block( js )
     if css.strip():
         code += inline_css_block( css )
+    code += ich
     args = {
         "mainscript": code
     }
     mkbasedir( outname )
-    with open( outname, "w" ) as f:
+    with codecs.open( outname, "w", "utf8" ) as f:
         f.write( pystache.render( template, args ) )
 
 def browserify_js():
     args = [ "browserify", "." ]
-    return sp.check_output( args ) 
+    return sp.check_output( args ).decode( "utf8" )
 
 def minify_js( gcjar, js ):
     args = [ "java", "-jar", gcjar, "--language_in=ECMASCRIPT5" ]
     p = sp.Popen( args, stdin = sp.PIPE, stdout = sp.PIPE )
-    output, _ = p.communicate( js )
-    return output
+    output, _ = p.communicate( js.encode("utf8") )
+    return output.decode( "utf8" )
 
-def main(gcjar, outname, templatefile, cssfile):
+def main(gcjar, outname, templatefile, ichdir, cssfile):
     fulljs = browserify_js()
     minjs = None
+    ich = inline_ich_blocks_from_dir( ichdir )
     for minify in (True,False):
         confs = [ "single-file" ]
         if minify:
@@ -61,7 +81,7 @@ def main(gcjar, outname, templatefile, cssfile):
         js = minjs if minify else fulljs
         css = contents( cssfile )
         template = contents( templatefile )
-        render_single_file( sfoutname, template, js, css )
+        render_single_file( sfoutname, template, ich, js, css )
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
--- src/Frontend.js
+++ src/Frontend.js
@@ -1,6 +1,13 @@
 var Stega = require("./Stega"),
+    ich = require("icanhaz"),
     $ = require( "jquery" );
 
 $( document ).on( "ready", function() {
-    $( "#shufflestega" ).html( "Ready!" );
+    var i = 0, root = $("#shufflestega");
+
+    root.html( "" );
+
+    for(i = 0; i < 52; i++) {
+        root.append( ich.entry( {number: i + 1} ) );
+    }
 } );
